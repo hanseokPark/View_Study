fcc0f4dd8358e89bb10f8a79c21aeaea447b88d0
hanseok Park <es12850@naver.com>
2018-04-19 01:03:32 +0900

    TEXTVIEW 활용

--- app/src/main/java/kr/or/dgit/it/view_study/TextViewExActivity.java
+++ app/src/main/java/kr/or/dgit/it/view_study/TextViewExActivity.java
@@ -4,24 +4,75 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class TextViewExActivity extends AppCompatActivity {
-    Button button;
+    EditText edittext;
+    CheckBox checkBox;
+    CharSequence result1;
+    CharSequence result2;
+    CharSequence result3;
+    RadioGroup radiogroup;
+    RadioButton radioButton01;
+    RadioButton radioButton02;
+    RadioButton radioButton03;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_view_ex);
         setTitle(getIntent().getStringExtra("title"));
 
-        /*button = findViewById(R.id.button);
-        button.setOnClickListener();*/
+        edittext = findViewById(R.id.editText);
+        checkBox = findViewById(R.id.checkText);
+        radiogroup = findViewById(R.id.radiogroup);
+        radioButton01 = findViewById(R.id.radioButton01);
+        radioButton02 = findViewById(R.id.radioButton02);
+        radioButton03 = findViewById(R.id.radioButton03);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    checkBox.setText("is Checked");
+                    result2 = checkBox.getText();
+                }else{
+                    checkBox.setText("is unChecked");
+                    result2 = checkBox.getText();
+                }
+            }
+        });
+
+        radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.radioButton01){
+                    result1 = radioButton01.getText();
+                }else if(checkedId == R.id.radioButton02){
+                    result1 = radioButton02.getText();
+                }else{
+                    result1 = radioButton03.getText();
+                }
+
+            }
+        });
+
+        result3 = edittext.getText();
+
     }
 
 
     public void button(View view) {
 
-        /*Toast.makeText(TextView.this, );*/
+
+        Toast.makeText(this, result3 + ", " + result2 +","+ result1, Toast.LENGTH_SHORT).show();
     }
 }
